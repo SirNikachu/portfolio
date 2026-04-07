42ef4c3a2a781ed8cba5db29e9fafce2e4e15aae
SirNikachu <nikkandula@gmail.com>
2026-04-07 01:08:50 -0500

    fix project page routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from './app/App';
+import App from "./app/App";
 import "./index.css";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <HashRouter>
-        <App />
+      <App />
     </HashRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
